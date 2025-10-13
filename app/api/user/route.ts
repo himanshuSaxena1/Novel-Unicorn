529de68906888b68650e49ce9d9276d8c7894d30
Himanshu Saxena <119810076+Theknight-1@users.noreply.github.com>
2025-10-13 17:33:22 +0530

    fixed build issues

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,9 @@
+export const dynamic = "force-dynamic";
+
+
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { SubscriptionAPI } from "@/lib/api";
 import { prisma } from "@/lib/prisma";
 
 export async function GET() {
